cea7324fcc210ee0ce0272d95ac152c137287c11
Sasha San <sansashadev@gmail.com>
2020-06-08 14:02:30 +0300

    Init

---
+++ config/application.rb
@@ -0,0 +1,11 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module RailsCourse
+  class Application < Rails::Application
+    config.load_defaults 6.0
+  end
+end
