362f36a0008aee9b013fa4f423e2263cf4fd9711
Alexander Batashov <abt@anadeainc.com>
2020-06-10 16:52:11 +0300

    REST ful contr part 3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,11 +24,32 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    unless (@item = Item.where(id: params[:id]).first)
+      render body: 'Page not found', status: 404
+    end
+  end
 
-  def update; end
+  def update
+    item = Item.where(id: params[:id]).first
+    if item.update(items_params)
+      redirect_to item_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
 
-  def destroy; end
+
+  def destroy
+    item = Item.where(id: params[:id]).first.destroy
+
+    if item.destroyed?
+      redirect_to items_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
 
   private
 
