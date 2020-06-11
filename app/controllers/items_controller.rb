b5d18e14d9f83e2204475c915792b6308ec36c91
Alexander Batashov <abt@anadeainc.com>
2020-06-11 20:46:54 +0300

    before_action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   layout false
   skip_before_action :verify_authenticity_token
+  before_action :find_item, only: %i[show edit update destroy]
+  before_action :admin?,    only: %i[edit update new create destroy]
+  after_action  :show_info, only: %i[index]
 
   def index
     @items = Item.all
@@ -19,20 +22,15 @@ class ItemsController < ApplicationController
   def new; end
 
   def show
-    unless (@item = Item.where(id: params[:id]).first)
-      render body: 'Page not found', status: 404
-    end
+    render body: 'Page not found', status: 404 unless @item
   end
 
   def edit
-    unless (@item = Item.where(id: params[:id]).first)
-      render body: 'Page not found', status: 404
-    end
+    render body: 'Page not found', status: 404 unless @item
   end
 
   def update
-    item = Item.where(id: params[:id]).first
-    if item.update(items_params)
+    if @item.update(items_params)
       redirect_to item_path
     else
       render json: item.errors, status: :unprocessable_entity
@@ -40,9 +38,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.where(id: params[:id]).first.destroy
-
-    if item.destroyed?
+    if @item.destroy.destroyed?
       redirect_to items_path
     else
       render json: item.errors, status: :unprocessable_entity
@@ -54,5 +50,17 @@ class ItemsController < ApplicationController
 
   def items_params
     params.permit(:name, :price)
+  end
+
+  def find_item
+    @item = Item.where(id: params[:id]).first
+  end
+
+  def admin?
+    render json: 'Access denied', status: :forbidden unless params[:admin]
+  end
+
+  def show_info
+    puts 'Index endpoint'
   end
 end
