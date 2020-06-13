f71ecc69d9cc15c93e95352c5d80d0a15cce621a
Alexander Batashov <abt@anadeainc.com>
2020-06-13 17:23:35 +0300

    has_and_belongs_to_many

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, presence: true
 
+  has_and_belongs_to_many :carts
+
   after_initialize { p 'initialize' } # Item.new Item.first
   after_save       { p 'save' }       # Item.create Item.save
   after_create     { p 'create' }     # Item.create
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_131628) do
+ActiveRecord::Schema.define(version: 2020_06_13_135153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2020_06_13_131628) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "carts_items", id: false, force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "item_id"
   end
 
   create_table "items", force: :cascade do |t|
