b360da69588d020a1613ce77f5b2eb7153c1974e
Alexander Batashov <abt@anadeainc.com>
2020-06-14 17:19:38 +0300

    layout

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  layout false
   skip_before_action :verify_authenticity_token
   before_action :find_item, only: %i[show edit update destroy upvote]
   before_action :admin?,    only: %i[edit]
@@ -45,7 +44,7 @@ class ItemsController < ApplicationController
     @item.increment! :votes_count
     redirect_to items_path
   end
-  
+
   def expensive
     @items = Item.where('price > 50')
     render :index
@@ -60,11 +59,6 @@ class ItemsController < ApplicationController
   def find_item
     @item = Item.where(id: params[:id]).first
     render_404 unless @item
-  end
-
-  def admin?
-    true
-    # render_403 unless params[:admin]
   end
 
   def show_info
