8854a01d81d104f6cda0e96f016b84b1c478410b
Alexander Batashov <abt@anadeainc.com>
2020-06-18 01:01:39 +0300

    ajax

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,7 +41,7 @@ class ItemsController < ApplicationController
   def destroy
     if @item.destroy.destroyed?
       flash[:success] = 'Item was deleted'
-      redirect_to items_path
+      render json: { success: true }
     else
       flash[:error] = "Item wasn't deleted"
       render json: item.errors, status: :unprocessable_entity
